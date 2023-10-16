f014d386e18c843712eb300d87677df6ee13951a
Leki <lekisintole82@gmail.com>
2023-10-16 15:47:22 +0300

    Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import AppsIcon from '@mui/icons-material/Apps';
+export default function Home() {
+  return (
+    <>
+    <div className="container">
+        <div className='header-left'>
+            <h1>Hoome</h1>
+            <h1>Store</h1>
+        </div>
+        <div className='header-right'>
+            <h1> Gmail </h1>
+            <h1> Images </h1>
+
+        </div>
+        <AppsIcon />
+    </div>
+    
+    
+    </>
+  )
+}
