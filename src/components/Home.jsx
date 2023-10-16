0a8fde4c6c53b0d4828d81130b7786f0ae6688dc
Leki <lekisintole82@gmail.com>
2023-10-16 18:12:15 +0300

    Home styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import AppsIcon from '@mui/icons-material/Apps';
+import React from "react";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Avatar } from "@mui/material";
+import { styled, Box } from "@mui/material";
+
+
+
+const HomeContainer= styled(Box)(()=>({
+  display :"flex",
+  flexDirection:"column",
+  height:"100vh"
+  }))
+
 export default function Home() {
   return (
-    <>
-    <div className="container">
-        <div className='header-left'>
-            <h1>Hoome</h1>
-            <h1>Store</h1>
+    <div className="HomeContainer">
+      <div className="header">
+        <div className="header-left">
+          <p>About</p>
+          <p>Store</p>
         </div>
-        <div className='header-right'>
-            <h1> Gmail </h1>
-            <h1> Images </h1>
-
+        <div className="header-right">
+          <p> Gmail </p>
+          <p> Images </p>
+          <AppsIcon />
+          <Avatar />
         </div>
-        <AppsIcon />
+      </div>
+      <div className="body">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
+          alt="google"
+        />
+      </div>
     </div>
-    
-    
-    </>
-  )
+  );
 }
