70f2b922a64d4a331d6f848587f84400269c9dc8
Leki <lekisintole82@gmail.com>
2023-10-17 08:33:47 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route}  from "react-router-dom";
 
+import SearchPage from './components/SearchPage';
 function App() {
   return (
-    <div className="App">
-        <Home />
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/search" element={<SearchPage/>}/>
+        </Routes>
+      </Router>
+        
     </div>
   );
 }
