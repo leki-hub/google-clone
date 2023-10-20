dca54580bf4094f4d0658bb54c1fc21a83c0763f
Leki <lekisintole82@gmail.com>
2023-10-20 15:35:43 +0300

    changes

--- src/useGoogle.js
+++ src/useGoogle.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react'
 
-const apiKey= "AIzaSyCwNSHXdUVtQ1pBE5RrF1YvhqNNyAk3T2Q"
-const apiCx= "264e76a229b3a4cfd"
-
 const  useGoogle= term=> {
     const [data , setData] = useState(null)
     useEffect(()=>{
